5381c89041c17dd5ca0a4897e18c39c0ae3ea49e
LAPTOP-69V583LH\SUNDAR <meenakshisundaram212000@gmail.com>
2024-04-27 23:33:09 +0530

    adding new url in api.js file

--- src/component/Config/api.js
+++ src/component/Config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-export const API_URL="https://full-stack-backend-production.up.railway.app"
+export const API_URL="https://full-stack-backend-production-3243.up.railway.app"
 
 export const api=axios.create({
     baseURL:API_URL,
